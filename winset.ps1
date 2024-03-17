4609f038611a7ae4474aeb9363711a5797d2f32c
aut0-m8 <144866024+aut0-m8@users.noreply.github.com>
2024-03-17 00:09:29 -0500

    Delete winset.ps1

--- winset.ps1
+++
@@ -1,5 +0,0 @@
-[Net.ServicePointManager]::SecurityProtocol =
-[Net.ServicePointManager]::SecurityProtocol -bor
-[Net.SecurityProtocolType]::Tls12
-
-irm https://raw.githubusercontent.com/aut0-m8/winset/main/winset.ps1 | iex
